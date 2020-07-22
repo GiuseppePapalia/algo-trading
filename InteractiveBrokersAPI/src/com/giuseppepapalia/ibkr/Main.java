d633fd2f3be16c58b32a1f75cb320c8a1320ef70
Julian <julianjohnmendoza@gmail.com>
2020-07-22 15:49:25 -0700

    Data in console

--- InteractiveBrokersAPI/src/com/giuseppepapalia/ibkr/Main.java
+++ InteractiveBrokersAPI/src/com/giuseppepapalia/ibkr/Main.java
@@ -1,13 +1,20 @@
 package com.giuseppepapalia.ibkr;
 
+import java.util.Date;
+
+import com.giuseppepapalia.ibkr.constants.BarSize;
+
 public class Main {
 
 	public static void main(String args[]) throws InterruptedException {
 		InteractiveBrokersClient client = new InteractiveBrokersClient(false, "DY1919358");
 		Thread.sleep(20000);
-		client.placeLongBracketOrder(InteractiveBrokersFactory.createStock("MSFT"), 5, 165.14, 168.14, 162.14);
+		// client.placeLongBracketOrder(InteractiveBrokersFactory.createStock("MSFT"),
+		// 5, 165.14, 168.14, 162.14);
+		Chart chart = client.getHistoricalData(InteractiveBrokersFactory.createStock("TSLA"),
+				new Date(System.currentTimeMillis()), "1 D", BarSize.ONE_MIN);
+		System.out.println(chart.getChart());
 		System.exit(0);
-
 	}
 
 }
